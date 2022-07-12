7cf436d8337c7f832c167ab65b0732190638e098
mojotron <igormalj@gmail.com>
2022-07-12 08:41:52 +0200

    Add intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+import "../styles/Intro.css";
+
+const Intro = () => {
+  return (
+    <div className="Intro">
+      <h1 className="Intro__heading">Quizzical</h1>
+      <p className="Intro__info">have fun answering trivia questions</p>
+      <button className="btn">Start quiz</button>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/__tests__/Intro.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import Intro from "../Intro";
+
+describe("Intro component", () => {
+  test("renders heading", () => {
+    render(<Intro />);
+    expect(screen.getByRole("heading").textContent).toBe("Quizzical");
+  });
+  test("renders info paragraph", () => {
+    render(<Intro />);
+    expect(
+      screen.getByText(/have fun answering trivia questions/i)
+    ).toBeInTheDocument();
+  });
+  test("renders button", () => {
+    render(<Intro />);
+    expect(
+      screen.getByRole("button", { name: /start quiz/i })
+    ).toBeInTheDocument();
+  });
+});
